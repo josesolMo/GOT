4ab2d05e17e49ed7cb0e72b62275d338a9575e64
Jose <joseandressolanomora@gmail.com>
2020-08-10 00:05:51 -0600

    HTTP Methods for Client

--- GOT-CLIENT/GOT-CLIENT/main.cpp
+++ GOT-CLIENT/GOT-CLIENT/main.cpp
@@ -1,11 +1,29 @@
 
 #include<iostream>
+#include<cpr/cpr.h>
 #include "ConsoleClient.h"
 
 using namespace std;
 
 int main() {
+	/*
 	ConsoleClient* client = ConsoleClient::getInstance();
 	client->thrd.join();
 	return 0;
+	*/
+	/*
+	{
+		std::cout << "Obteniendo datos..." << std::endl;
+		auto r = cpr::Get(cpr::Url{ "https://localhost:44348/api/archivo" });
+		std::cout << "Respuesta: " << r.text << std::endl;
+	}
+	*/
+	{
+		std::cout << "Action: Post Archivo" << std::endl;
+		auto r = cpr::Post(cpr::Url{ "http://localhost:44348/api/archivo" },
+			cpr::Body{ R"({"IDArchivo":"<id>", 
+            "DireccionArchivo":"<dir>","DataArchivo":"<data>","NombreArchivo":"<name>"})" },
+			cpr::Header{ { "Content-Type", "application/json" } });
+		std::cout << "Returned Status:" << r.status_code << std::endl;
+	}
 }
