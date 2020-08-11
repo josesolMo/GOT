72f9a4b9e10ca2e83a6c7d1ffc4b08c0daaa05c2
Joseph JImenez <josephdjz@estudiantec.cr>
2020-08-11 08:49:26 -0600

    Adding List for Huffman Obj
    Added List and node for huff obj

---
+++ GOT-Server/HuffmanCompres/HuffmanCompres/LinkedNode.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HuffmanCompres
+{
+    class LinkedNode
+    {
+        public LinkedNode next { get; set; }
+        public CodigoHuffman<char> data {get; set;}
+
+    }
+}
---
+++ GOT-Server/HuffmanCompres/HuffmanCompres/SingleList.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HuffmanCompres
+{
+    class SingleList
+    {
+        private LinkedNode head;
+        public void printAllNodes()
+        {
+            LinkedNode current = head;
+            while (current != null)
+            {
+                Console.WriteLine(value: current.data);
+                current = current.next;
+            }
+        }
+        public void Add(CodigoHuffman<char> objetoHuffman)
+        {
+            LinkedNode toAdd = new LinkedNode();
+            toAdd.data = objetoHuffman;
+            LinkedNode current = head;
+            current.next = toAdd;
+        }
+        public void addEnd(CodigoHuffman<char> objetoHuffman)
+        {
+            if (head == null)
+            {
+                head = new LinkedNode();
+                head.data = objetoHuffman;
+                head.next = null;
+            }
+            else
+            {
+                LinkedNode toAdd = new LinkedNode();
+                toAdd.data = objetoHuffman;
+
+                LinkedNode current = head;
+                while (current.next != null)
+                {
+                    current = current.next;
+                }
+                current.next = toAdd;
+            }
+        }
+        public void Delete(CodigoHuffman<char> objetoHuffman)
+        {
+            LinkedNode node = head;
+            LinkedNode currNode = head;
+            LinkedNode prevNode = null;
+            while (node != null)
+            {
+                currNode = node;
+                if (node.data == objetoHuffman)
+                {
+                    if (prevNode != null)
+                    {
+                        prevNode.next = currNode.next;
+                    }
+                    else
+                    {
+                        head = head.next;
+                    }
+                    break;
+                }
+                prevNode = currNode;
+                node = node.next;
+            }
+        }
+
+    }
+}
